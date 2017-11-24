ee1ebb2799ebf326fa1bdf97910a6e8bbb8187bf
semyonchick <semyonchick@gmail.com>
2017-11-24 12:18:02 +0500

    hyperscript

--- init.js
+++ init.js
@@ -1,5 +1,6 @@
 requirejs.config({
-    baseUrl: 'https://smartsam.ru/bx24_plus/lib'
+    baseUrl: 'https://smartsam.ru/bx24_plus/lib',
+    urlArgs: "v=201724111211"
 });
 
 requirejs(['mailchimp', 'hyperscript']);
