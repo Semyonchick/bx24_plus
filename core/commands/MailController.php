ff5933381008ed5f9d45c6bf409a6a63989ccdd5
semyonchick <semyonchick@gmail.com>
2017-12-21 09:29:36 +0500

    автооплата по счетам в битрикс

--- core/commands/MailController.php
+++ core/commands/MailController.php
@@ -40,24 +40,29 @@ class MailController extends Controller
 
         for ($i = 0; $i < $count; $i++) {
             $msg = $storage->getMessage($i + 1);
-            if ($msg->getHeaders()->getFrom() == 'bank@ubrr.ru') {
-                $text = strip_tags($msg->getMsgBody());
-                if (preg_match('#(\d{20}).+?\s([\d\s\.]+)\s.+\n?От:\s(.+)\n?Ostatok po schetu#', $text, $match)) {
-                    list(, $bill, $price, $from) = $match;
-                    $price = (float)str_replace(' ', '', $price);
-
-                    if (($data = $this->bx('crm.requisite.bankdetail.list', ['filter' => ['RQ_ACC_NUM' => $bill], 'select' => ['ENTITY_ID']])) &&
-                        ($requisiteFrom = $this->findRequisite($from)) &&
-                        ($requisiteTo = $this->findRequisite($data[0]['ENTITY_ID'], ['ID'])) &&
-                        ($bill = $this->selectBill(['UF_COMPANY_ID' => $requisiteFrom['ENTITY_ID'], 'UF_MYCOMPANY_ID' => $requisiteTo['ENTITY_ID'], 'PAYED' => 'N'], $price))) {
-                        $this->payBill($bill, $price);
-                        $storage->removeMessage($msg->id);
-                    } else {
-                        $this->toAdmin('Не найдено счетов для оплаты ' . $price);
-                    }
-                }
+            if ($msg->getHeaders()->getFrom() == 'bank@ubrr.ru' && $this->parseMail(strip_tags($msg->getMsgBody()))) {
+                $storage->removeMessage($msg->id);
             }
         }
+    }
+
+    public function parseMail($text)
+    {
+        if (preg_match('#(\d{20}).+?\s([\d\s\.]+)\s.+\n?От:\s(.+)\n?Ostatok po schetu#', $text, $match)) {
+            list(, $bill, $price, $from) = $match;
+            $price = (float)str_replace(' ', '', $price);
+
+            if (($data = $this->bx('crm.requisite.bankdetail.list', ['filter' => ['RQ_ACC_NUM' => $bill], 'select' => ['ENTITY_ID']])) &&
+                ($requisiteFrom = $this->findRequisite($from)) &&
+                ($requisiteTo = $this->findRequisite($data[0]['ENTITY_ID'], ['ID'])) &&
+                ($bill = $this->selectBill(['UF_COMPANY_ID' => $requisiteFrom['ENTITY_ID'], 'UF_MYCOMPANY_ID' => $requisiteTo['ENTITY_ID'], 'PAYED' => 'N'], $price))) {
+                $this->payBill($bill, $price);
+                return true;
+            } else {
+                $this->toAdmin('Не найдено счетов для оплаты ' . $price);
+            }
+        }
+        return false;
     }
 
     public function bx($method, $get = null, $post = null)
