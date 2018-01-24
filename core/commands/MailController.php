1068de7496c579522cb0cad26d528bd6c015a0ca
semyonchick <semyonchick@gmail.com>
2018-01-24 14:26:11 +0500

    автооплата по счетам в битрикс

--- core/commands/MailController.php
+++ core/commands/MailController.php
@@ -10,6 +10,7 @@ namespace app\commands;
 use app\components\BX24;
 use Eden\Mail\Imap;
 use yii\console\Controller;
+use yii\helpers\Console;
 
 /**
  * @author Qiang Xue <qiang.xue@gmail.com>
@@ -43,7 +44,22 @@ class MailController extends Controller
 
         $imap->setActiveMailbox('INBOX');
 
-        $emails = $imap->search(['FROM "bank@ubrr.ru"'], 0, 5, false, true);
+        $emails = $imap->search(['FROM "emails.tinkoff.ru"'], 0, 5, false, true);
+        Console::output('tinkoff: ' . count($emails));
+        foreach ($emails as $email) foreach ($email['attachment'] as $attach) if ($attach['text/plain']) {
+            $result = \Yii::$app->cache->get($cacheId = 'mail' . $email['uid']);
+            if ($result === false) {
+                foreach (['cp1251', 'koi8-r'] as $fromEncode) {
+                    $text = iconv($fromEncode, 'utf8', $attach['text/plain']);
+                    if ($text) break;
+                }
+                $result = $this->parse1CFile($text, $email['uid']);
+                if ($result) \Yii::$app->cache->set($cacheId, $result, 86400 * 365);
+            }
+        }
+
+        $emails = $imap->search(['FROM "ubrr.ru"'], 0, 5, false, true);
+        Console::output('ubrr: ' . count($emails));
         foreach ($emails as $email) {
             $result = \Yii::$app->cache->get($cacheId = 'mail' . $email['uid']);
             if ($result === false) {
@@ -54,56 +70,35 @@ class MailController extends Controller
         }
 
         $imap->disconnect();
+
+        if (var_dump(date('i')) == '33') {
+            $this->actionCheckData();
+        }
     }
 
-    public function parseMail($text, $emailId = false)
+    public function parse1CFile($text, $emailId = false)
     {
-        $this->_payBills = 0;
+        $result = [];
+        foreach (explode('СекцияДокумент=Платежное поручение', $text) as $i => $row) {
+            if ($this->getData($emailId)) continue;
+            if ($i && preg_match_all('#(.+)\=(.+)#', $row, $matches)) {
+                $data = array_combine($matches[1], array_map('trim', $matches[2]));
 
-        if (preg_match('#(\d{2}\.\d{2}\.\d{2}\s\d{2}\:\d{2}).+(\d{20}).+?\s([\d\s\.]+)\s.+\n?От:\s(.+?)(?:(?:р\/с|Р\/С).+)?\n?Ostatok po schetu#iu', $text, $match)) {
-            if ($emailId && $this->bx('lists.element.get', ['IBLOCK_TYPE_ID' => 'lists', 'IBLOCK_ID' => 45, 'ELEMENT_CODE' => $emailId])) return true;
+                if (!($requisiteFrom = $this->findRequisite($data['ПлательщикИНН'])))
+                    $this->toAdmin('Не найден плательщик');
+                if (!($requisiteTo = $this->findRequisite($data['ПолучательИНН'])))
+                    $this->toAdmin('Не найден получатель');
 
-            list(, $date, $bill, $price, $from) = $match;
-            $price = (float)str_replace(' ', '', $price);
-
-            $requisiteTo = \Yii::$app->cache->get($bill);
-            if ($requisiteTo === false) {
-                $requisiteTo = ($data = $this->bx('crm.requisite.bankdetail.list', ['filter' => ['RQ_ACC_NUM' => $bill], 'select' => ['ENTITY_ID']])) ? $this->findRequisite($data[0]['ENTITY_ID'], ['ID']) : null;
-                \Yii::$app->cache->set($bill, $requisiteTo, 86400);
+                $result[] = $this->addData($data['Плательщик'], $data['Дата'], $data['Сумма'], $requisiteFrom['ENTITY_ID'], $requisiteTo['ENTITY_ID'], $emailId);
             }
-
-            if ($requisiteFrom = $this->findRequisite($from)) {
-                if ($requisiteTo) {
-                    if (!$this->config['skipBilling']) {
-                        if ($bill = $this->selectBill(['UF_COMPANY_ID' => $requisiteFrom['ENTITY_ID'], 'UF_MYCOMPANY_ID' => $requisiteTo['ENTITY_ID'], 'PAYED' => 'N'], $price)) {
-                            $this->payBill($bill, $price);
-                        } else {
-                            $this->toAdmin('Не найдено счетов на сумму ' . $price);
-                        }
-                    }
-                } else {
-                    $this->toAdmin('Не найден счет получателя ' . $bill);
-                }
-            } else {
-                $this->toAdmin('Не найден плательщик ' . $from);
-            }
-
-            // Регистрируем список
-            return $this->bx('lists.element.add', [], [
-                'IBLOCK_TYPE_ID' => 'lists',
-                'IBLOCK_ID' => '45',
-                'ELEMENT_CODE' => $emailId ?: uniqid(),
-                'FIELDS' => [
-                    'ACTIVE_FROM' => preg_replace('#\.(\d{2})\s#', '.20$1 ', $date),
-                    'NAME' => $from,
-                    'PROPERTY_171' => $price,
-                    'PROPERTY_173' => $requisiteFrom['ENTITY_ID'],
-                    'PROPERTY_175' => $requisiteTo['ENTITY_ID'],
-                    'PROPERTY_177' => $this->config['skipBilling'] ? $price : $this->_payBills,
-                ],
-            ]);
         }
-        return false;
+        return $result;
+    }
+
+    public function getData($emailId)
+    {
+        return $emailId && $this->bx('lists.element.get', ['IBLOCK_TYPE_ID' => 'lists', 'IBLOCK_ID' => 45, 'ELEMENT_CODE' => $emailId]);
+
     }
 
     public function bx($method, $get = null, $post = null)
@@ -112,7 +107,7 @@ class MailController extends Controller
         return $this->_bx->run($method, $get, $post);
     }
 
-    public function findRequisite($value, $columns = ['NAME', 'RQ_COMPANY_NAME', 'RQ_COMPANY_FULL_NAME'])
+    public function findRequisite($value, $columns = ['NAME', 'RQ_INN', 'RQ_COMPANY_NAME', 'RQ_COMPANY_FULL_NAME'])
     {
         foreach ($columns as $column) {
             $filter = [$column => mb_strtoupper(trim($value))];
@@ -124,8 +119,43 @@ class MailController extends Controller
         return false;
     }
 
-    public function selectBill($filter, $price)
+    public function toAdmin($message)
     {
+        mail('semyonchick@gmail.com', 'From bitrix controller', $message);
+        return false;
+    }
+
+    public function addData($from, $date, $price, $fromId, $toId, $emailId)
+    {
+        $this->_payBills = 0;
+
+        if ($toId && $fromId && !$this->config['skipBilling']) {
+            if ($bill = $this->selectBill($fromId, $toId, $price)) {
+                $this->payBill($bill, $price);
+            } else {
+                $this->toAdmin('Не найдено счетов на сумму ' . $price);
+            }
+        }
+
+        // Регистрируем список
+        return $this->bx('lists.element.add', [], [
+            'IBLOCK_TYPE_ID' => 'lists',
+            'IBLOCK_ID' => '45',
+            'ELEMENT_CODE' => $emailId ?: uniqid(),
+            'FIELDS' => [
+                'ACTIVE_FROM' => $date,
+                'NAME' => $from,
+                'PROPERTY_171' => $price,
+                'PROPERTY_173' => $fromId,
+                'PROPERTY_175' => $toId,
+                'PROPERTY_177' => $this->config['skipBilling'] ? $price : $this->_payBills,
+            ],
+        ]);
+    }
+
+    public function selectBill($fromId, $toId, $price)
+    {
+        $filter = ['UF_COMPANY_ID' => $fromId, 'UF_MYCOMPANY_ID' => $toId, 'PAYED' => 'N'];
         $filter['!%ACCOUNT_NUMBER'] = '#';
         $bills = $this->bx('crm.invoice.list', ['order' => ['ID' => 'ASC'], 'filter' => $filter, 'select' => ['ID', 'PRICE', 'UF_CRM_1513599139']]);
 
@@ -174,9 +204,67 @@ class MailController extends Controller
             $this->toAdmin('Не удалось оплатить счет ' . $bill['ID'] . ' на сумму ' . $pay);
     }
 
-    public function toAdmin($message)
+    public function parseMail($text, $emailId = false)
     {
-        mail('semyonchick@gmail.com', 'From bitrix controller', $message);
+        if (preg_match('#(\d{2}\.\d{2}\.\d{2}\s\d{2}\:\d{2}).+(\d{20}).+?\s([\d\s\.]+)\s.+\n?От:\s(.+?)(?:(?:р\/с|Р\/С).+)?\n?Ostatok po schetu#iu', $text, $match)) {
+            if ($this->getData($emailId)) return true;
+
+            list(, $date, $bill, $price, $from) = $match;
+            $price = (float)str_replace(' ', '', $price);
+
+            $requisiteTo = \Yii::$app->cache->get($bill);
+            if ($requisiteTo === false) {
+                $requisiteTo = ($data = $this->bx('crm.requisite.bankdetail.list', ['filter' => ['RQ_ACC_NUM' => $bill], 'select' => ['ENTITY_ID']])) ? $this->findRequisite($data[0]['ENTITY_ID'], ['ID']) : null;
+                if (!$requisiteTo) {
+                    $this->toAdmin('Не найден счет получателя ' . $bill);
+                } else {
+                    \Yii::$app->cache->set($bill, $requisiteTo, 86400);
+                }
+            }
+
+            if (!($requisiteFrom = $this->findRequisite($from))) {
+                $this->toAdmin('Не найден плательщик ' . $from);
+            }
+
+            return $this->addData($from, preg_replace('#\.(\d{2})\s#', '.20$1 ', $date), $price, $requisiteFrom['ENTITY_ID'], $requisiteTo['ENTITY_ID'], $emailId);
+        }
         return false;
+    }
+
+    public function actionCheckData()
+    {
+        $forPay = [];
+        $data = $this->bx('lists.element.get', ['IBLOCK_TYPE_ID' => 'lists', 'IBLOCK_ID' => 45, 'ELEMENT_ORDER' => ['ID' => 'DESC']]);
+        foreach ($data as $row) {
+            if ($row['PROPERTY_173'] && $row['PROPERTY_175'] && current($row['PROPERTY_171']) > current($row['PROPERTY_177'])) {
+                $forPay[] = $row;
+            }
+        }
+
+        if (count($forPay)) {
+            Console::output(count($forPay));
+
+            array_reverse($forPay);
+            foreach ($forPay as $row) {
+                $this->_payBills = 0;
+
+                $price = current($row['PROPERTY_171']) - current($row['PROPERTY_177']);
+                if ($bill = $this->selectBill(current($row['PROPERTY_173']), current($row['PROPERTY_175']), $price)) {
+                    $this->payBill($bill, $price);
+                }
+
+                if ($this->_payBills) {
+                    $row['PROPERTY_177'] = $this->_payBills;
+                    $this->bx('lists.element.update', [], [
+                        'IBLOCK_TYPE_ID' => 'lists',
+                        'IBLOCK_ID' => '45',
+                        'ELEMENT_ID' => $row['ID'],
+                        'FIELDS' => $row + [
+                                'PROPERTY_177' => $this->_payBills,
+                            ],
+                    ]);
+                }
+            }
+        }
     }
 }
