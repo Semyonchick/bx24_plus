2b8451f71d4ea14f707b8f57b71c7de348745799
semyonchick <semyonchick@gmail.com>
2018-10-08 15:04:33 +0500

    rere контроль рабочего времени

--- webhook/timeman.php
+++ webhook/timeman.php
@@ -21,7 +21,7 @@ $postJSON = file_get_contents('php://input');
 
 $result = [];
 
-$bx = new BX24(['url'=>$_GET['c']]);
+$bx = new BX24(['url' => Config::$timeman[$_GET['c']]]);
 
 $result += $_GET;
 
@@ -34,11 +34,11 @@ $users = [
 $data = [
     'USER_ID' => $users[$_GET['u']],
     'REPORT' => 'from server api',
-    'TIME' => str_replace('00:00', '05:00', date('c', strtotime($_GET['d']))),
+//    'TIME' => str_replace('00:00', '05:00', date('c', strtotime($_GET['d']))),
 ];
 
-{
-    if ($_GET['ip'] && stripos($_GET['ip'], '192.168.1.') !== false) $_GET['ip'] = '78.159.225.99';
+if ($_GET['ip']) {
+    if (stripos($_GET['ip'], '192.168.1.') !== false) $_GET['ip'] = '78.159.225.99';
     $geo = \Yii::$app->cache->get('sypexgeo' . $_GET['ip']);
     if ($geo === false) {
         $geo = json_decode(file_get_contents('http://api.sypexgeo.net/json/' . $_GET['ip']), 1);
@@ -48,14 +48,18 @@ $data = [
         'LAT' => $geo['city']['lat'],
         'LON' => $geo['city']['lon'],
     ];
+    $data['IP_OPEN'] = $_GET['ip'];
 }
 
-if (strpos($row[1], 'Logon') !== false) {
-    $result = $this->get('timeman.open', $data+ ['IP_OPEN' => $_GET['ip']]);
+if ($_GET['s'] == 'logon') {
+    $result = $bx->run('timeman.open', $data);
 }
-if (strpos($row[1], 'Logoff') !== false) {
-    $result = $this->get('timeman.close', $data + ['IP_CLOSE' => $_GET['ip']]);
+if ($_GET['s'] == 'logoff') {
+    $result = $bx->run('timeman.pause', $data);
 }
+
+mail('semyonchick@gmail.com', 'WorkTrack', print_r([$_GET, $data, $result], 1));
+
 
 header('Content-Type: application/json');
 echo json_encode([
